9ee4914e491742609a472bedcb565160b52e3c5c
Felipe <videirafelipe020@gmail.com>
2024-07-29 14:17:41 -0300

    toggle button style

--- src/pages/Boards/Boards.tsx
+++ src/pages/Boards/Boards.tsx
@@ -7,6 +7,7 @@ import useTheme from "../../hooks/useTheme";
 function Boards() {
   // const { boardId, taskId } = useParams();
   const [boardName, setBoardName] = useState("Platform Launch");
+  const [boards, setBoards] = useState(new Array(7).fill("Projeto Kanban"));
   const { theme, toggleTheme } = useTheme();
 
   return (
@@ -17,16 +18,48 @@ function Boards() {
           htmlFor="sidebar-toggle"
           className="boards__sidebar-toggle-label"
         >
-          Hide Sidebar
+          <img
+            src="/assets/icon-hide-sidebar.svg"
+            alt="Hide"
+            width={15}
+            height={15}
+            className="boards__sidebar-toggle-hide-icon"
+          />
+          <img
+            src="/assets/icon-show-sidebar.svg"
+            alt="Show"
+            width={15}
+            height={10}
+            className="boards__sidebar-toggle-show-icon"
+          />
+          <span className="boards__sidebar-toggle-text">Hide Sidebar</span>
         </label>
 
         <div className="boards__sidebar-content">
-          {/* <p>All boards (3)</p> */}
-          <ul className="boards__sidebar-options">
-            <li>Projeto Kanban</li>
-            <li>Projeto Mandan</li>
-            <li>Projeto Gandan</li>
-          </ul>
+          <img
+            src="/assets/logo-light.svg"
+            alt="Kanban"
+            width={150}
+            className="--dark-theme"
+          />
+          <img
+            src="/assets/logo-dark.svg"
+            alt="Kanban"
+            width={150}
+            className="--light-theme"
+          />
+
+          <div className="boards__sidebar-options">
+            <h3 className="boards__sidebar-options-title">{`All boards (${boards.length})`}</h3>
+            <ul>
+              {boards.map((item) => (
+                <li>{item}</li>
+              ))}
+            </ul>
+            <button type="button" className="boards__sidebar-create-button">
+              Create new board
+            </button>
+          </div>
 
           <button
             onClick={toggleTheme}
