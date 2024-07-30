a1359e22c67340db6b26dec5ff3988e9c1aba851
Felipe <videirafelipe020@gmail.com>
2024-07-30 16:03:26 -0300

    sidebar component

--- src/pages/Boards/Boards.tsx
+++ src/pages/Boards/Boards.tsx
@@ -1,16 +1,17 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-import { SetStateAction, useState } from "react";
 import "./Boards.scss";
-import useTheme from "../../hooks/useTheme";
+import { useCallback, useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
 // import { useParams } from "react-router-dom";
+
+type Board = { id: string; title: string };
 
 function Boards() {
   // const { boardId, taskId } = useParams();
-  const [selectedBoard, setSelectedBoard] = useState({
+  const [selectedBoard, setSelectedBoard] = useState<Board>({
     id: "1",
     title: "Platform Launch",
   });
-  const [boards, setBoards] = useState([
+  const [boards, setBoards] = useState<Board[]>([
     {
       id: "1",
       title: "Platform Launch",
@@ -24,85 +25,22 @@ function Boards() {
       title: "Platform Daman",
     },
   ]);
-  const { theme, toggleTheme } = useTheme();
 
-  function selectBoard(board: { title: string }) {
+  const selectBoard = useCallback((board: Board) => {
     setSelectedBoard(board);
-  }
+  }, []);
 
   return (
-    <div className="boards">
-      <div className="boards__menu">
-        <input type="checkbox" id="menu-toggle" defaultChecked />
-        <label htmlFor="menu-toggle" className="boards__menu-toggle-label">
-          <img
-            src="/assets/icon-hide-sidebar.svg"
-            alt="Hide"
-            width={15}
-            height={15}
-            className="boards__menu-toggle-hide-icon"
-          />
-          <img
-            src="/assets/icon-show-sidebar.svg"
-            alt="Show"
-            width={15}
-            height={10}
-            className="boards__menu-toggle-show-icon"
-          />
-          <span className="boards__menu-toggle-text">Hide sidebar</span>
-        </label>
-
-        <div className="boards__menu-content">
-          <img
-            src="/assets/logo-light.svg"
-            alt="Kanban"
-            width={150}
-            className="boards__menu-logo --dark-theme"
-          />
-          <img
-            src="/assets/logo-dark.svg"
-            alt="Kanban"
-            width={150}
-            className="boards__menu-logo --light-theme"
-          />
-
-          <div className="boards__menu-opts">
-            <h3 className="boards__menu-opts-title">{`All boards (${boards.length})`}</h3>
-            <div className="boards__menu-opts-list">
-              {boards.map((board) => (
-                <button
-                  type="button"
-                  className={`boards__menu-opts-list-item ${
-                    selectedBoard.id === board.id
-                      ? "boards__menu-opts-list-item--selected"
-                      : ""
-                  }`}
-                  onClick={() => selectBoard(board)}
-                >
-                  <img src="/assets/icon-board.svg" alt="Board" width={15} />
-                  {board.title}
-                </button>
-              ))}
-            </div>
-            <button type="button" className="boards__menu-create-button">
-              Create new board
-            </button>
-          </div>
-
-          <button
-            onClick={toggleTheme}
-            className="boards__theme-toggle"
-            type="button"
-          >
-            toggle
-          </button>
-        </div>
-      </div>
-
-      <div className="boards__toolbar">
+    <div className="container">
+      <Sidebar
+        boards={boards}
+        selectedBoard={selectedBoard}
+        onSelectboard={selectBoard}
+      />
+      <div className="toolbar">
         <h1>{selectedBoard.title}</h1>
       </div>
-      <div className="boards__tasks" />
+      <div className="main" />
     </div>
   );
 }
---
+++ src/pages/Boards/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import "./Sidebar.scss";
+import useTheme from "../../../../hooks/useTheme";
+import BoardIcon from "../../../../icons/icon-board.svg?react";
+
+function Sidebar({ boards, selectedBoard, onSelectboard }) {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className="sidebar">
+      <input type="checkbox" id="sidebar-toggle" defaultChecked />
+      <label htmlFor="sidebar-toggle" className="sidebar__toggle-label">
+        <img
+          src="/assets/icon-hide-sidebar.svg"
+          alt="Hide"
+          width={15}
+          height={15}
+          className="sidebar__toggle-hide-icon"
+        />
+        <img
+          src="/assets/icon-show-sidebar.svg"
+          alt="Show"
+          width={15}
+          height={10}
+          className="sidebar__toggle-show-icon"
+        />
+        <span className="sidebar__toggle-text">Hide sidebar</span>
+      </label>
+
+      <div className="sidebar__content">
+        <img
+          src="/assets/logo-light.svg"
+          alt="Kanban"
+          width={150}
+          className="sidebar__logo --dark-theme"
+        />
+        <img
+          src="/assets/logo-dark.svg"
+          alt="Kanban"
+          width={150}
+          className="sidebar__logo --light-theme"
+        />
+
+        <div className="sidebar__opts">
+          <h3 className="sidebar__opts-title">{`All boards (${boards.length})`}</h3>
+          {boards.map((board) => (
+            <button
+              type="button"
+              className={`sidebar__opts-btn sidebar__opts-btn--list-item ${
+                selectedBoard.id === board.id
+                  ? "sidebar__opts-btn--selected"
+                  : ""
+              }`}
+              onClick={() => onSelectboard(board)}
+            >
+              <BoardIcon /> {board.title}
+            </button>
+          ))}
+          <button
+            type="button"
+            className="sidebar__opts-btn sidebar__opts-btn--create"
+          >
+            <BoardIcon />+ Create new board
+          </button>
+        </div>
+        <div className="sidebar__theme-toggle-container">
+          <img
+            src="/assets/icon-light-theme.svg"
+            alt="Light theme"
+            width={16}
+          />
+          <label className="sidebar__theme-toggle">
+            <input
+              type="checkbox"
+              checked={theme === "dark"}
+              onClick={toggleTheme}
+            />
+            <span />
+          </label>
+          <img src="/assets/icon-dark-theme.svg" alt="Dark theme" width={16} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
