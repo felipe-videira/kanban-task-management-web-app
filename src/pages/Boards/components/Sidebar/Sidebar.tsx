868e30905578a0389c1d9a4410aa9d637f84972f
Felipe <videirafelipe020@gmail.com>
2024-08-02 10:39:44 -0300

    toolbar layout adjustments

--- src/pages/Boards/components/Sidebar/Sidebar.tsx
+++ src/pages/Boards/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import "./Sidebar.scss";
-import { arrayOf, func, object, shape, string } from "prop-types";
+import { arrayOf, func, shape, string } from "prop-types";
 import useTheme from "../../../../hooks/useTheme";
 import BoardIcon from "../../../../icons/icon-board.svg?react";
+import HideIcon from "../../../../icons/icon-hide-sidebar.svg?react";
+import ShowIcon from "../../../../icons/icon-show-sidebar.svg?react";
+import LogoIcon from "../../../../icons/logo-light.svg?react";
+import LightThemeIcon from "../../../../icons/icon-light-theme.svg?react";
+import DarkThemeIcon from "../../../../icons/icon-dark-theme.svg?react";
 
 function Sidebar({
   boards,
@@ -19,39 +22,18 @@ function Sidebar({
   return (
     <div className="sidebar">
       <label htmlFor="sidebar-toggle" className="sidebar__toggle-label">
-        <img
-          src="/assets/icon-hide-sidebar.svg"
-          alt="Hide"
-          width={15}
-          height={15}
-          className="sidebar__toggle-hide-icon"
-        />
-        <img
-          src="/assets/icon-show-sidebar.svg"
-          alt="Show"
-          width={15}
-          height={10}
-          className="sidebar__toggle-show-icon"
-        />
+        <HideIcon className="sidebar__toggle-hide-icon" />
+        <ShowIcon className="sidebar__toggle-show-icon" />
+
         <span className="sidebar__toggle-text">Hide sidebar</span>
       </label>
 
       <div className="sidebar__content">
-        <img
-          src="/assets/logo-light.svg"
-          alt="Kanban"
-          width={150}
-          className="sidebar__logo --dark-theme"
-        />
-        <img
-          src="/assets/logo-dark.svg"
-          alt="Kanban"
-          width={150}
-          className="sidebar__logo --light-theme"
-        />
+        <LogoIcon className="sidebar__logo" />
 
         <div className="sidebar__opts">
           <h3 className="sidebar__opts-title">{`All boards (${boards.length})`}</h3>
+
           {boards.map((board) => (
             <button
               key={board.id}
@@ -67,10 +49,14 @@ function Sidebar({
                 htmlFor="sidebar-toggle"
                 className="sidebar__opts-toggle-label"
               >
-                <BoardIcon /> {board.title}
+                <span className="sidebar__opts-board-icon">
+                  <BoardIcon />
+                </span>
+                {board.title}
               </label>
             </button>
           ))}
+
           <button
             type="button"
             className="sidebar__opts-btn sidebar__opts-btn--create"
@@ -79,11 +65,8 @@ function Sidebar({
           </button>
         </div>
         <div className="sidebar__theme-toggle-container">
-          <img
-            src="/assets/icon-light-theme.svg"
-            alt="Light theme"
-            width={16}
-          />
+          <LightThemeIcon />
+
           <label className="sidebar__theme-toggle">
             <input
               type="checkbox"
@@ -92,7 +75,8 @@ function Sidebar({
             />
             <span />
           </label>
-          <img src="/assets/icon-dark-theme.svg" alt="Dark theme" width={16} />
+
+          <DarkThemeIcon />
         </div>
       </div>
     </div>
--- src/pages/Boards/components/Toolbar/Toolbar.tsx
+++ src/pages/Boards/components/Toolbar/Toolbar.tsx
@@ -1,25 +1,24 @@
 import "./Toolbar.scss";
 import { shape, string } from "prop-types";
+import LogoIcon from "../../../../icons/logo-light.svg?react";
+import LogoMobileIcon from "../../../../icons/logo-mobile.svg?react";
+import ChevronIcon from "../../../../icons/icon-chevron-down.svg?react";
 
 function Toolbar({ selectedBoard }: { selectedBoard: Board }) {
   return (
     <div className="toolbar">
+      <div className="toolbar__logo-wrapper">
+        <LogoIcon className="toolbar__logo " />
+      </div>
+
       <label htmlFor="sidebar-toggle" className="toolbar__sidebar-toggle-label">
-        <img
-          src="/assets/logo-mobile.svg"
-          width={24}
-          alt="Kanban"
-          className="toolbar__logo-mobile"
-        />
+        <LogoMobileIcon className="toolbar__logo-mobile" />
+
         {selectedBoard && (
           <h1 className="toolbar__title">{selectedBoard.title}</h1>
         )}
-        <img
-          src="/assets/icon-chevron-up.svg"
-          width={10}
-          alt="Chevron"
-          className="toolbar__chevron"
-        />
+
+        <ChevronIcon className="toolbar__chevron" />
       </label>
     </div>
   );
