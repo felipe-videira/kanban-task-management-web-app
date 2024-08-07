8945c5d9cbd2d27fb4aa3f2d43c4024480ac5a0c
Felipe <videirafelipe020@gmail.com>
2024-08-07 17:26:40 -0300

    modal component

--- src/pages/Boards/Boards.tsx
+++ src/pages/Boards/Boards.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/Main";
 import Toolbar from "./components/Toolbar/Toolbar";
 import { isMobile } from "../../utils";
+import Modal from "../../components/Modal/Modal";
 // import { useParams } from "react-router-dom";
 const mockBoards = [
   {
@@ -23,35 +24,55 @@ function Boards() {
   // const { boardId, taskId } = useParams();
   const [selectedBoard, setSelectedBoard] = useState<Board>();
   const [boards, setBoards] = useState<Board[]>([]);
+  const [showBoardModal, setShowBoardModal] = useState<boolean>(false);
 
   const selectBoard = useCallback((board: Board) => {
     setSelectedBoard(board);
   }, []);
 
   const addTask = useCallback(() => {}, []);
-  const createBoard = useCallback(() => {}, []);
-  const editBoard = useCallback(() => {}, []);
+  const createBoard = useCallback(() => {
+    setShowBoardModal(true);
+  }, []);
+  const editBoard = useCallback(() => {
+    console.log("aqsjq");
+    setShowBoardModal(true);
+  }, []);
   const deleteBoard = useCallback(() => {}, []);
 
   useEffect(() => {
-    setBoards(mockBoards);
+    // setBoards(mockBoards);
   }, []);
 
   return (
     <div className="container">
       <input type="checkbox" id="sidebar-toggle" defaultChecked={!isMobile()} />
+
       <Sidebar
         boards={boards}
         selectedBoard={selectedBoard}
         onSelectBoard={selectBoard}
       />
+
       <Toolbar
         selectedBoard={selectedBoard}
         onAddTask={addTask}
         onEditBoard={editBoard}
         onDeleteBoard={deleteBoard}
       />
+
       <Main boards={boards} onCreateBoard={createBoard} />
+
+      <Modal show={showBoardModal}>
+        <button
+          type="button"
+          onClick={() => {
+            setShowBoardModal(false);
+          }}
+        >
+          close
+        </button>
+      </Modal>
     </div>
   );
 }
